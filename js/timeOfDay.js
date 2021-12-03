c42d7082e3a578428bd5607e09ec0bcacb957fb4
Ido Tanne <idovader@gmail.com>
2021-12-03 11:34:30 -0500

    Adjusted code to switch icons based on Time of Day

--- js/timeOfDay.js
+++ js/timeOfDay.js
@@ -7,5 +7,14 @@ function getDayTime(hours) {
 }
 
 $(function() {
-    document.body.className = getDayTime(new Date().getHours());
+    tod = getDayTime(new Date().getHours())
+    document.body.className = tod;
+    document.getElementById("HBO").src = "imgs/"+tod+"/HBO Max.png";
+    document.getElementById("YouTubeTV").src = "imgs/"+tod+"/YouTubeTV.png";
+    document.getElementById("Hulu").src = "imgs/"+tod+"/Hulu.png";
+    document.getElementById("Netflix").src = "imgs/"+tod+"/Netflix.png";
+    document.getElementById("Youtube").src = "imgs/"+tod+"/Youtube.png";
+    document.getElementById("Disney+").src = "imgs/"+tod+"/Disney+.png";
+    document.getElementById("Plex").src = "imgs/"+tod+"/Plex.png";
+    document.getElementById("Twitch").src = "imgs/"+tod+"/Twitch.png";
 });
