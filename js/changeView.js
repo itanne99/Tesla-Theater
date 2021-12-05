449333a570f0a04da3e9137482e49143cc83b798
Ido Tanne <idovader@gmail.com>
2021-12-04 23:05:50 -0500

    Reverted Site to original view. (Unresponsive)

--- js/changeView.js
+++ js/changeView.js
@@ -1,6 +1,6 @@
 $(document).ready(function($) {
     let ww = document.body.clientWidth;
-    if (ww<1201){
+    if (ww<1111){
         $("#mainView").removeClass('vh-100')
     } else {
         $("#mainView").addClass('vh-100')
